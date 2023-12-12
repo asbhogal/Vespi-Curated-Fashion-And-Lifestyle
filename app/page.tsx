89ec330236039d1adbf2584f627561d35d5c33e8
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-12 01:03:55 +0000

    Adds further styles and positioning to Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import HomePageCarousel from "@/components/HomePageCarousel";
+import { Header } from "@/stories/Header";
 
 export default function Home() {
-  return <HomePageCarousel />;
+  return (
+    <>
+      <HomePageCarousel />
+    </>
+  );
 }
