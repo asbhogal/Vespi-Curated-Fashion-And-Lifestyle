920167b1372e61e49e5f61441a61a1cab9727179
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-13 13:54:51 +0000

    Reverts API route back to Next.js types

--- app/api/IG/route.ts
+++ app/api/IG/route.ts
@@ -1,40 +1,37 @@
-import { rest } from "msw";
+import { NextResponse } from "next/server";
 
-export const handlers = [
-  rest.get("/api/IG", (req, res, ctx) => {
-    const instagramPosts = [
-      {
-        id: 1,
-        imgSrc: "1.jpg",
-        imgAlt:
-          "A small pile of clothes neatly folded, resting on a white chair",
-      },
-      {
-        id: 2,
-        imgSrc: "2.jpg",
-        imgAlt:
-          "A young man in blue blazer and sweater and tartan-style trousers standing in front of a building",
-      },
-      {
-        id: 3,
-        imgSrc: "3.jpg",
-        imgAlt:
-          "A young woman wearing a reverse dark-blue cap and oversized biege sweater sitting on a stool",
-      },
-      {
-        id: 4,
-        imgSrc: "4.jpg",
-        imgAlt:
-          "A middle-aged black man with a beard, red beanie and black turtleneck against a rural backdrop",
-      },
-      {
-        id: 5,
-        imgSrc: "5.jpg",
-        imgAlt:
-          "A young woman wearing a dark blue checkered jacket standing in the street overlooking her shoulder",
-      },
-    ];
+export async function GET(request: Request) {
+  const instagramPosts = [
+    {
+      id: 1,
+      imgSrc: "1.jpg",
+      imgAlt: "A small pile of clothes neatly folded, resting on a white chair",
+    },
+    {
+      id: 2,
+      imgSrc: "2.jpg",
+      imgAlt:
+        "A young man in blue blazer and sweater and tartan-style trousers standing in front of a building",
+    },
+    {
+      id: 3,
+      imgSrc: "3.jpg",
+      imgAlt:
+        "A young woman wearing a reverse dark-blue cap and oversized biege sweater sitting on a stool",
+    },
+    {
+      id: 4,
+      imgSrc: "4.jpg",
+      imgAlt:
+        "A middle-aged black man with a beard, red beanie and black turtleneck against a rural backdrop",
+    },
+    {
+      id: 5,
+      imgSrc: "5.jpg",
+      imgAlt:
+        "A young woman wearing a dark blue checkered jacket standing in the street overlooking her shoulder",
+    },
+  ];
 
-    return res(ctx.status(200), ctx.json({ data: instagramPosts }));
-  }),
-];
+  return NextResponse.json({ data: instagramPosts }, { status: 200 });
+}
