82e2ee0fdc5b954cbf22617b3a5295b5cec100a4
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-16 21:53:15 +0000

    Adds IG slider component

--- components/IGFeed.tsx
+++ components/IGFeed.tsx
@@ -1,3 +1,10 @@
+// @ts-ignore
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import Image from "next/image";
+import "@splidejs/react-splide/css";
+import { Buttons } from "./Button";
+import { pt_serif } from "@/lib/types";
+
 type IGPostType = {
   id: number;
   imgSrc: string;
@@ -22,13 +29,55 @@ export default async function Page() {
   try {
     const posts = await getData();
     return (
-      <main>
-        {posts.data.map((post) => (
-          <ul key={post.id}>
-            <li>{post.imgSrc}</li>
-          </ul>
-        ))}
-      </main>
+      <div className="grid gap-8 overflow-hidden">
+        <div className="flex justify-between">
+          <h2 className={["text-[1.3125rem]", pt_serif.className].join(" ")}>
+            On the gram
+          </h2>
+          <Buttons
+            icon
+            label="Follow us on the gram"
+            className="hidden sm:flex"
+          />
+        </div>
+        <section>
+          <Splide
+            options={{
+              type: "loop",
+              perPage: 5,
+              perMove: 1,
+              autoplay: true,
+              loop: true,
+              gap: "1.5rem",
+              breakpoints: {
+                1024: {
+                  perPage: 4,
+                },
+                768: {
+                  perPage: 3,
+                },
+              },
+            }}
+            aria-label="instagram posts"
+          >
+            {posts.data.map((post) => (
+              <SplideSlide key={post.id}>
+                <Image
+                  src={`/images/IGPost-${post.imgSrc}`}
+                  alt={post.imgSrc}
+                  width={200}
+                  height={200}
+                />
+              </SplideSlide>
+            ))}
+          </Splide>
+        </section>
+        <Buttons
+          icon
+          label="Follow us on the gram"
+          className="flex sm:hidden"
+        />
+      </div>
     );
   } catch (error) {
     console.error("Error fetching data:");
