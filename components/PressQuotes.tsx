d26b11e15d7dddc16ace94145df2ef92b8f19f57
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-17 17:01:40 +0000

    Adds further styling and responsiveness

--- components/PressQuotes.tsx
+++ components/PressQuotes.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function PressQuotes() {
   return (
-    <div>
-      <h2 className="press-quote">
+    <div className="border-t border-b border-[#e2e2e2] max-w-[1920px] w-full mx-auto">
+      <h2 className="press-quote lg:pt-36 lg:pb-[5.625rem] md:pt-[7.8125rem] md:pb-[5.375rem] pt-[4.7656rem] pb-[3.5625rem] px-[1.875rem]">
         “Beautiful collection of indie American brands at a fair price”
       </h2>
       <div className="grid grid-cols-[auto_auto_auto_auto] gap-[30px] md:gap-20 items-center max-w-[41.125rem] w-full mx-auto mt-[3.5625rem] md:mt-20">
