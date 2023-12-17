989fb04554e35dab5f0c381f7de17fa9a3963e69
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-17 15:08:03 +0000

    Adds react hook form and Zod validation

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+});
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values);
+}
 export default function Subscribe() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-0 max-w-[960px] w-full">
       <div className="grid gap-2">
@@ -31,34 +58,52 @@ export default function Subscribe() {
           Unsubscribe any time.
         </p>
       </div>
-      <form className="relative inline-block">
-        <Input
-          className="max-w-80 w-full h-[3.625rem] rounded-none border border-black placeholder:text-black placeholder:text-[1.0625rem] placeholder:font-serif"
-          placeholder="Your email"
-        ></Input>
-        <Button
-          className="bg-transparent hover:bg-transparent rounded-none absolute right-[.3125rem] top-2"
-          aria-labelledby="submit-email"
+      <Form {...form}>
+        <form
+          className="relative inline-block"
+          onSubmit={form.handleSubmit(onSubmit)}
         >
-          <span id="submit-email" hidden>
-            Submit Email
-          </span>
-          <svg
-            width="12"
-            height="7"
-            viewBox="0 0 12 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">Email Address</FormLabel>
+                <FormControl>
+                  <Input
+                    className="max-w-80 w-full h-[3.625rem] rounded-none border border-black placeholder:text-black placeholder:text-[1.0625rem] placeholder:font-serif"
+                    placeholder="Your email"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+            className="bg-transparent hover:bg-transparent rounded-none absolute right-[.3125rem] top-2"
+            aria-labelledby="submit-email"
+            type="submit"
           >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M9.29286 4.50004L7.6464 6.14643L8.3535 6.85355L10.8535 4.3536L11.2071 4.00004L10.8536 3.64648L8.35355 1.14648L7.64645 1.85359L9.29289 3.50004L0 3.50004V4.50004L9.29286 4.50004Z"
-              fill="black"
-            />
-          </svg>
-        </Button>
-      </form>
+            <span id="submit-email" hidden>
+              Submit Email
+            </span>
+            <svg
+              width="12"
+              height="7"
+              viewBox="0 0 12 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.29286 4.50004L7.6464 6.14643L8.3535 6.85355L10.8535 4.3536L11.2071 4.00004L10.8536 3.64648L8.35355 1.14648L7.64645 1.85359L9.29289 3.50004L0 3.50004V4.50004L9.29286 4.50004Z"
+                fill="black"
+              />
+            </svg>
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
