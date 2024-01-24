3c7ca53583995b8eaa62d864812782f608ea11cd
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2024-01-24 16:51:24 +0000

    Installs and configs Playwright (incl. a11y testing)

---
+++ tests/a11y.spec.ts
@@ -0,0 +1,13 @@
+import test, { expect } from "@playwright/test";
+import AxeBuilder from '@axe-core/playwright';
+
+
+test.describe("homepage", () => {
+  test("should not have any automatically detectable accessibility issues", async ({ page }) => {
+    await page.goto("/");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  })
+})
